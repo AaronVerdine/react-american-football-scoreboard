9b2cafe9dda64c2aa9f4fc0f4fa375aab5d895ba
Your name <a.m.verdine@gmail.com>
2019-10-16 19:19:48 -0400

    Clean up file structure, create HomeTeam.js to contain Home Team info & score

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, { useState } from "react";
 import "./App.css";
+
+import HomeTeam from "./HomeTeam";
 import BottomRow from "./BottomRow";
 
 function App() {
@@ -13,14 +15,13 @@ function App() {
   return (
     <div className="container">
       <section className="scoreboard">
+
         <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
+          <HomeTeam />
+        </div>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{homeScore}</div>
-          </div>
+            
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
---
+++ src/HomeTeam.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function HomeTeam() {
+
+    return (
+        <div className="Home">
+            <h2 className="home_name">Lions</h2>
+            <h2 className="home_score">{homeScore}</h2>
+        </div>
+    );
+
+}
+
+
+export default HomeTeam
